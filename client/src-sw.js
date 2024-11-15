dedeb44ed843a5cfd971587d3bc82bf52b2446e4
alstonjohnson <johnson.alston@icloud.com>
2024-11-15 10:44:42 -0600

    updated src-sw.js file to implement asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,23 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: "asset-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
 registerRoute();
